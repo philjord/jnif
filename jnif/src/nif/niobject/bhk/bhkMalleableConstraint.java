90a4c8d6bb93e3c87baa3127c74741859d307cca
phil <philjord@ihug.co.nz>
2016-03-31 16:47:31 +0200

    bhkMalleableConstraint fixed a bit

--- jnif/src/nif/niobject/bhk/bhkMalleableConstraint.java
+++ jnif/src/nif/niobject/bhk/bhkMalleableConstraint.java
@@ -5,6 +5,7 @@ import java.nio.ByteBuffer;
 
 import nif.ByteConvert;
 import nif.NifVer;
+import nif.basic.NifPtr;
 import nif.basic.NifRef;
 import nif.compound.NifHingeDescriptor;
 import nif.compound.NifLimitedHingeDescriptor;
@@ -16,7 +17,7 @@ public class bhkMalleableConstraint extends bhkConstraint
 	/**
 	 
 	 <niobject name="bhkMalleableConstraint" abstract="0" inherit="bhkConstraint">
-
+	
 	 A malleable constraint.
 	 
 	 <add name="Type" type="uint">Type of constraint.</add>
@@ -36,13 +37,11 @@ public class bhkMalleableConstraint extends bhkConstraint
 
 	public int type;
 
-	public int unknownInt2;
+	public int NumEntities;
 
-	public NifRef unknownLink1;
+	public NifPtr[] Entities;
 
-	public NifRef unknownLink2;
-
-	public int unknownInt3;
+	public int Priority;
 
 	public NifHingeDescriptor hinge;
 
@@ -52,36 +51,57 @@ public class bhkMalleableConstraint extends bhkConstraint
 
 	public float tau;
 
-	public byte unknownByte1;
-
 	public float damping;
+
+	public float strength;
 
 	public boolean readFromStream(ByteBuffer stream, NifVer nifVer) throws IOException
 	{
 		boolean success = super.readFromStream(stream, nifVer);
 		type = ByteConvert.readInt(stream);
-		unknownInt2 = ByteConvert.readInt(stream);
-		unknownLink1 = new NifRef(NiObject.class, stream);
-		unknownLink2 = new NifRef(NiObject.class, stream);
-		unknownInt3 = ByteConvert.readInt(stream);
-		if (type == 1)
+		NumEntities = ByteConvert.readInt(stream);
+		Entities = new NifPtr[NumEntities];
+		for (int i = 0; i < NumEntities; i++)
+			Entities[i] = new NifPtr(NiObject.class, stream);
+
+		Priority = ByteConvert.readInt(stream);
+		if (type == 0)
+		{
+			//hinge = new NifHingeDescriptor(stream, nifVer);
+			new Throwable("Ball and socket descriptor to do!").printStackTrace();
+		}
+		else if (type == 1)
 		{
 			hinge = new NifHingeDescriptor(stream, nifVer);
-		}
-		else if (type == 7)
-		{
-			ragdoll = new NifRagdollDescriptor(stream, nifVer);
 		}
 		else if (type == 2)
 		{
 			limitedHinge = new NifLimitedHingeDescriptor(stream, nifVer);
 		}
-		tau = ByteConvert.readFloat(stream);
+		else if (type == 6)
+		{
+			//hinge = new NifHingeDescriptor(stream, nifVer);
+			new Throwable("Prismatic descriptor to do!").printStackTrace();
+		}
+		else if (type == 7)
+		{
+			ragdoll = new NifRagdollDescriptor(stream, nifVer);
+		}
+		else if (type == 8)
+		{
+			//hinge = new NifHingeDescriptor(stream, nifVer);
+			new Throwable("Stiff Spring descriptor to do!").printStackTrace();
+		}
+		if (nifVer.LOAD_VER <= NifVer.VER_20_0_0_5)
+		{
+			tau = ByteConvert.readFloat(stream);
+			damping = ByteConvert.readFloat(stream);
+		}
 		if (nifVer.LOAD_VER >= NifVer.VER_20_2_0_7)
 		{
-			unknownByte1 = ByteConvert.readByte(stream);
+			strength = ByteConvert.readFloat(stream);
 		}
-		damping = ByteConvert.readFloat(stream);
+
 		return success;
 	}
 }
