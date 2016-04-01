2e011737cce5da755465da7eee00e865cd9bc885
phil <philjord@ihug.co.nz>
2016-04-01 14:28:17 +0200

    Binary extra data dumped once converted to tangents and bitangents

--- jnif/src/nif/niobject/NiBinaryExtraData.java
+++ jnif/src/nif/niobject/NiBinaryExtraData.java
@@ -17,6 +17,8 @@ public class NiBinaryExtraData extends NiExtraData
 	 </niobject>
 	 */
 
+	
+	//Note once this is converted to somethign else, it will be set to null!
 	public NifByteArray binaryData;
 
 	public boolean readFromStream(ByteBuffer stream, NifVer nifVer) throws IOException
